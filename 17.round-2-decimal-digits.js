4efcde5e08bb1d2302071d843065b8335acc4e84
tilavallisir931 <tilavalli.sir931@gmail.com>
2022-05-13 10:40:41 +0530

    round to 2 decimal places

---
+++ 17.round-2-decimal-digits.js
@@ -0,0 +1,7 @@
+// Write a function that takes a number (a) as argument
+// Round a to the 2nd digit after the comma
+// Return the rounded number
+function myFunction(a)
+{
+return a.toFixed(2)-'0';
+}
